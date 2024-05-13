24a3b5a4359cdc8a4008fbad8a1588aeb76ffb5f
shur4le <161835756+shur4le@users.noreply.github.com>
2024-05-13 20:21:41 +0300

    Delete student.sql

--- student.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE student(
-  id INT PRIMARY KEY,
-  name VARCHAR2(50) NOT NULL,
-  id_group INT
-  );
-
-ALTER TABLE student
-ADD CONSTRAINT conn_to_group
-FOREIGN KEY (id_group) REFERENCES group(id);
